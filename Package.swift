c4818ed1deed76d5ad834cbb16f62ccc59c5bca1
Nikhil Pardasani <nikhil_pardasani@yahoo.com>
2025-03-18 20:15:11 +0530

    Initial Commit for SPM

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WindowsAzureMessagingNotifications",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "WindowsAzureMessagingNotifications",
+            targets: ["WindowsAzureMessagingNotifications"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "WindowsAzureMessaging",
+            path: "Sources/WindowsAzureMessagingNotifications/WindowsAzureMessaging_rebuilt.xcframework"
+        ),
+        .target(
+            name: "WindowsAzureMessagingNotifications",
+            dependencies: [
+                "WindowsAzureMessaging"
+            ],
+            path: "Sources"
+        ),
+
+    ]
+)
